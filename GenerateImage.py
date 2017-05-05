b7b0a5c4a5c6eea66abbabd9a60276493a5c1747
Michael Lo <nz.ampersand@gmail.com>
2017-05-06 11:29:28 +1200

    Added some comments and documentation

--- GenerateImage.py
+++ GenerateImage.py
@@ -3,17 +3,18 @@ from socket import timeout
 from urllib.error import HTTPError, URLError
 
 from PIL import Image
-# TODO: Send HTTP requests asynchronously
+# TODO: Send HTTP requests asynchronously?
 
 
 class APIRequest:
     def __init__(self, url):
         self.url = url
-        # TODO: Add customisable param tuples instead of hardcoding into url using urllib.urlencode
+        # TODO: Add customisable param tuples using urllib.urlencode instead of hardcoding into url
         return
 
 
 def main():
+    # Random.org's API has a quota of how much data you're allowed to use. If exceeded, all requests will return 503
     if check_quota():
         print('Quota is fine')
         generate_random_image(128, 128)
@@ -23,13 +24,16 @@ def main():
 
 def generate_random_image(width, height):
     num_pixels = width * height
-    raw_values = generate_multiple_int(0, 255, num_pixels * 3)
 
+    # Generate three random values per pixel in the image
+    raw_values = generate_int_values(0, 255, num_pixels * 3)
+
+    # Split the response array into three equal sets
     r_values = raw_values[:num_pixels]
     g_values = raw_values[num_pixels:num_pixels * 2]
     b_values = raw_values[num_pixels * 2:]
 
-    image = Image.new('RGB', (width, height), 'green')
+    image = Image.new('RGB', (width, height))
     raw_pixels = image.load()
     n = 0
 
@@ -39,20 +43,15 @@ def generate_random_image(width, height):
             raw_pixels[i, j] = (r_values[n], g_values[n], b_values[n]) #TODO: Add alpha channel?
             n += 1
 
+    # Display the generated image and write it to a file on disk
     image.show()
     image.save('Output.bmp', 'bmp')
 
 
-def generate_single_int(rand_min, rand_max):
-    response = send_request(APIRequest('https://www.random.org/integers/?min=' + str(rand_min) + '&max=' + str(rand_max)
-                                       + '&num=1&col=1&format=plain&base=10&rnd=new'))
-
-    return int(response)
-
-
-def generate_multiple_int(rand_min, rand_max, num_generated):
+def generate_int_values(rand_min, rand_max, num_generated):
     output = ''
 
+    # Can request a maximum of 10,000 numbers at a time via the API, so loop and split the request until we have enough
     while num_generated != 0:
         if num_generated < 10000:
             num_to_request = num_generated
@@ -70,6 +69,7 @@ def generate_multiple_int(rand_min, rand_max, num_generated):
     return list(map(int, output.split()))
 
 
+# Returns the count of how many bits I'm still allowed to request today
 def check_quota():
     response = send_request(APIRequest('https://www.random.org/quota/?format=plain'))
     decoded = int(response)
